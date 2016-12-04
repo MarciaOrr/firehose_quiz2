9877b45573a0298da5defa36c20394bd52392aea
MarciaOrr <orrmar@swcp.com>
2016-12-04 02:40:06 +0000

    Had error with validates in models/dog.rb where could not add record to DB. Create record works again.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,6 @@
 class Dog < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 40, minimum: 2 }
+  validates :breed, length: { maximum: 60, minimum: 3 }
+  validates :nickname, length: {maximum: 40 }
+  validates :story, length: { maximum: 1000}
 end
