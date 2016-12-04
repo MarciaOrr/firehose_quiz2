14f61dda5f1e4e429a9f6f3a122d76af3fee12bb
MarciaOrr <orrmar@swcp.com>
2016-12-04 01:29:03 +0000

    Very basic index page. Add dog button with modal. Database create working

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -4,4 +4,14 @@ class DogsController < ApplicationController
     @dogs = Dog.all
   end
 
+  def create
+    @dog = Dog.create(dog_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :nickname, :birthday, :story)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'dogs#index'
 
+  resources :dogs
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
