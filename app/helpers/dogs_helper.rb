ada0e46001a189a395cfc7ca77d5ddd2b0a8a8e7
MarciaOrr <orrmar@swcp.com>
2016-12-05 18:15:03 +0000

    Added helper to calculate age from birthday

--- app/helpers/dogs_helper.rb
+++ app/helpers/dogs_helper.rb
@@ -1,2 +1,24 @@
 module DogsHelper
+
+  def age_string(dob)
+    today = Date.today
+    age = 0
+    age_string = ''
+    if dob.year < today.year
+      age = today.year - dob.year
+      if dob.month > today.month
+        age -= 1
+      elsif dob.month == today.month && dob.day > today.day
+        age -= 1
+      end
+      return age.to_s + " years old"
+
+    elsif
+      age = today.month - dob.month
+      if dob.day > today.day
+        age -= 1
+      end
+      return age.to_s + " months old"
+    end
+  end
 end
